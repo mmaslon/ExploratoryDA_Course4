3704f5346739467f095b7dd9632a9794e964eef3
mmaslon <magdamaslon@yahoo.com>
2019-03-19 18:43:52 +0000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -16,6 +16,7 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 #plot3
 #Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
+EmissionsBaltimore<-subset(NEI,fips=="24510")
 q3<-EmissionsBaltimore%>%group_by(type,year)%>%summarise(emissions_per_type=sum(Emissions))%>%ungroup()
 #axis treated as discrete variable (rather than continous)
 q3$year<-as.factor(q3$year)
